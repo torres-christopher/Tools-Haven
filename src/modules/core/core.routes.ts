6c1a2b886901e2312990eab4a1b16ca1ff9df915
Christopher Torres <73254689+torres-christopher@users.noreply.github.com>
2026-06-15 13:18:31 +0000

    Updated URLs to only work on supported locales

--- src/modules/core/core.routes.ts
+++ src/modules/core/core.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import healthRouter from './health/health.routes.js'
 import mainRouter from './home/home.routes.js'
 // import legalRouter from './legal/legal.routes.js' // Unused because now it is all in czech
+import { supportedLocales, type SupportedLocale } from '../../shared/types/supportedLocale.js'
 import { getMain, getFAQ } from './home/home.controller.js'
 import { getContact, getPrivacy, getTerms } from './legal/legal.controller.js'
 import { getAllTools, getSitemap, getRobots } from './site.controller.js'
@@ -15,7 +16,15 @@ router.get('/sitemap.xml', getSitemap)
 router.get('/robots.txt', getRobots)
 
 // Set i18next language from URL param for all /:lang routes
-router.param('lang', (req, _res, next, lang) => {
+router.param('lang', (req, res, next, lang) => {
+  // Check that /:lang is of supported local (/vsechny-nastroje fix)
+  if (!supportedLocales.includes(lang as SupportedLocale)) {
+    res.status(404).render('errors/error', {
+      title: '404',
+      message: 'Stránka nenalezena',
+    })
+    return
+  }
   req.i18n.changeLanguage(lang)
   next()
 })
