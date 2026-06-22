d12d8d595f82b95a65dad9579a5efbc8e8e28378
Christopher Torres <73254689+torres-christopher@users.noreply.github.com>
2026-06-22 14:06:13 +0000

    Date calculator schema

--- src/modules/tools/datetime/date-calculator/date-calculator.schema.ts
+++ src/modules/tools/datetime/date-calculator/date-calculator.schema.ts
@@ -2,38 +2,49 @@ import { z } from 'zod'
 
 // Range between dates
 export const dateCalculatorRangeInput = z.object({
-  startDate: z.date(),
-  endDate: z.date(),
+  startDate: z.coerce.date(),
+  endDate: z.coerce.date(),
 })
 
 // Add/Substract dates// Range between dates
 export const dateCalculatorArithmeticInput = z.object({
-  startDate: z.date(),
-  type: z.enum(['add', 'substract']),
-  days: z.number(),
-  weeks: z.number(),
-  months: z.number(),
-  years: z.number(),
+  startDate: z.coerce.date(),
+  type: z.enum(['add', 'subtract']),
+  days: z.coerce.number().nonnegative(),
+  weeks: z.coerce.number().nonnegative(),
+  months: z.coerce.number().nonnegative(),
+  years: z.coerce.number().nonnegative(),
 })
 
 // Week number
 export const dateCalculatorWeekInput = z.object({
-  inputDate: z.date(),
+  inputDate: z.coerce.date(),
 })
 
 export const dateCalculatorRangeOutput = z.object({
-  days: z.number(),
-  weeks: z.number(),
-  months: z.number(),
-  years: z.number(),
+  daysTotal: z.number().nonnegative(),
+  daysRemainder: z.number().nonnegative(),
+  weeksTotal: z.number().nonnegative(),
+  weeksRemainder: z.number().nonnegative(),
+  monthsTotal: z.number().nonnegative(),
+  monthsRemainder: z.number().nonnegative(),
+  yearsTotal: z.number().nonnegative(),
 })
 
 export const dateCalculatorArithmeticOutput = z.object({
   outputDate: z.date(),
+  weekday: z.number().min(1).max(7),
 })
 
 // Week number
 export const dateCalculatorWeekOutput = z.object({
-  week: z.number(),
-  year: z.number(),
+  week: z.number().nonnegative().max(53),
+  year: z.number().nonnegative(),
 })
+
+export type DateCalculatorRangeInput = z.infer<typeof dateCalculatorRangeInput>
+export type DateCalculatorArithmeticInput = z.infer<typeof dateCalculatorArithmeticInput>
+export type DateCalculatorWeekInput = z.infer<typeof dateCalculatorWeekInput>
+export type DateCalculatorRangeOutput = z.infer<typeof dateCalculatorRangeOutput>
+export type DateCalculatorArithmeticOutput = z.infer<typeof dateCalculatorArithmeticOutput>
+export type DateCalculatorWeekOutput = z.infer<typeof dateCalculatorWeekOutput>
