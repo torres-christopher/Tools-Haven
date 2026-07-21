feb25120c530055b724e9d5ba1dd88f91360fd09
Christopher Torres <73254689+torres-christopher@users.noreply.github.com>
2026-07-21 13:04:34 +0000

    Schema for age calculator done

---
+++ src/modules/tools/datetime/age-calculator/age-calculator.schema.ts
@@ -0,0 +1,28 @@
+import { z } from 'zod'
+
+export const ageCalculatorInput = z.object({
+  birthDate: z.coerce.date(),
+})
+
+export const ageCalculatorOutput = z.object({
+  age: z.object({
+    // Complete age
+    days: z.number().nonnegative(),
+    months: z.number().nonnegative(),
+    years: z.number().nonnegative(),
+  }),
+  birthday: z.object({
+    // Time to birthday
+    days: z.number().min(1),
+    weekday: z.number().min(1).max(7),
+  }),
+  birthdate: z.object({
+    // Birthdate metadata
+    weekday: z.number().min(1).max(7), // Week day
+    day: z.number().nonnegative().max(366), // Day number in year
+    week: z.number().nonnegative().max(53), // Week number in year
+  }),
+})
+
+export type AgeCalculatorInput = z.infer<typeof ageCalculatorInput>
+export type AgeCalculatorOutput = z.infer<typeof ageCalculatorOutput>
